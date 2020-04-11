d1ffcf6094e48d1a3a3818362084f1c4ba7a9f96
Rahul Malhotra <rahulmalhotra9620@yahoo.in>
2020-04-11 19:23:07 +0530

    Initial Commit

---
+++ force-app/main/default/lwc/platformEventToast/platformEventToast.js
@@ -0,0 +1,60 @@
+/*
+*	Author:- Rahul Malhotra
+*	Description:- Platform Event Toast
+*	Created:- 11/04/2020
+*	Last Updated:- 11/04/2020
+*	Code Origin:- SFDCStop (https://www.sfdcstop.com/)
+*/
+import { LightningElement, api } from 'lwc';
+import { subscribe, unsubscribe, onError } from 'lightning/empApi';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import currentUserId from '@salesforce/user/Id';
+
+export default class PlatformEventToast extends LightningElement {
+
+    @api toastKey;
+    @api toastVariant;
+    @api toastMode;
+    @api runInSystemMode;
+    channelName = '/event/ToastEvent__e';
+    subscription = {};
+
+    connectedCallback() {
+        const ci = this;
+        const toastCallback = function(response) {
+            let toastData = response['data']['payload'];
+            if(
+                toastData &&
+                toastData['Key__c'] &&
+                (toastData['Key__c'] === ci.toastKey) &&
+                (
+                    ci.runInSystemMode ||
+                    (toastData['CreatedById'] === currentUserId)
+                )
+            ) {
+                const toastEvent = new ShowToastEvent({
+                    title: toastData['Title__c'],
+                    message: toastData['Message__c'],
+                    variant: toastData['Variant__c'] ? toastData['Variant__c'] : ci.toastVariant,
+                    mode: toastData['Mode__c'] ? toastData['Mode__c'] : ci.toastMode
+                });
+                ci.dispatchEvent(toastEvent);
+            }
+        }
+        subscribe(this.channelName, -1, toastCallback).then(response => {
+            console.log('Subscribed to Platform Event Toast');
+            this.subscription = response;
+        });
+        onError(error => {
+            console.log('Error in Platform Event Toast');
+            console.log(error);
+        });
+    }
+
+    disconnectedCallback() {
+        unsubscribe(this.subscription, response => {
+            console.log('Un-Subscribed from Platform Event Toast');
+            console.log(response);
+        });
+    }
+}
